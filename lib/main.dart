57bb2df2bd378b16596a13f219592580ab24c1b7
omarbabou <obabou50@gmail.com>
2024-09-24 16:39:13 +0200

    Implementing meals screen style and functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/categories.dart';
 
+
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
@@ -23,7 +24,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const CategoriesScreen()
     );
   }
 }
